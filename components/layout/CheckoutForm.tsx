beebb26abc329af4f2e741b189e8fc2a3f7b2ed2
PFelipe15 <paulofelipet15@gmail.com>
2024-06-28 10:04:50 -0300

    adicionadno Number type

--- components/layout/CheckoutForm.tsx
+++ components/layout/CheckoutForm.tsx
@@ -1,8 +1,12 @@
-'use client';
+"use client";
 
 import { formatPrice } from "@/lib/formatPrice";
- import { useCartStore } from "@/store";
-import { PaymentElement, useStripe, useElements } from "@stripe/react-stripe-js"
+import { useCartStore } from "@/store";
+import {
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 
 function CheckoutForm({ clientSecret }: { clientSecret: string }) {
@@ -12,7 +16,7 @@ function CheckoutForm({ clientSecret }: { clientSecret: string }) {
   const cartStore = useCartStore();
 
   const totalPrice = cartStore.cart.reduce((acc, item) => {
-    return acc + item.price! * item.quantity!;
+    return acc + Number(item.price!) * item.quantity!;
   }, 0);
 
   const formattedPrice = formatPrice(totalPrice);
@@ -20,37 +24,38 @@ function CheckoutForm({ clientSecret }: { clientSecret: string }) {
   useEffect(() => {
     if (!stripe) return;
     if (!clientSecret) return;
+  }, [clientSecret, stripe]);
 
-  }, [clientSecret, stripe])
-  
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!stripe || !elements) return;
 
     setLoading(true);
-    stripe.confirmPayment({
-      elements,
-      redirect: "if_required"
-    }).then((result) => {
-      if (!result.error) {
-        cartStore.setCheckout("success");
-      }
-      setLoading(false);
-    })
-    
-  }
+    stripe
+      .confirmPayment({
+        elements,
+        redirect: "if_required",
+      })
+      .then((result) => {
+        if (!result.error) {
+          cartStore.setCheckout("success");
+        }
+        setLoading(false);
+      });
+  };
   return (
     <form onSubmit={handleSubmit} id="payment-form">
-      <PaymentElement id="payment-element" options={{ layout: 'tabs'}} />
+      <PaymentElement id="payment-element" options={{ layout: "tabs" }} />
       <h1 className="py-4 font-bold">Total: {formattedPrice}</h1>
       <button
         type="submit"
         disabled={!stripe || isLoading}
-        className="bg-teal-600 text-white py-2 px-4 rounded-md">
-        {isLoading ? 'Carregando...' : 'Finalizar compra'}
+        className="bg-teal-600 text-white py-2 px-4 rounded-md"
+      >
+        {isLoading ? "Carregando..." : "Finalizar compra"}
       </button>
     </form>
-  )
+  );
 }
 
 export default CheckoutForm;
