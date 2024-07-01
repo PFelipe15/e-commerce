4c8b808366765fe058fb0d3925f2006e7f4a9a67
PFelipe15 <paulofelipet15@gmail.com>
2024-07-01 12:22:55 -0300

    types line components incluido

--- app/(admin)/admin/_components/line-chart-component.tsx
+++ app/(admin)/admin/_components/line-chart-component.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-export default function LineChartComponent({ data, dataKey }) {
+export default function LineChartComponent({ data, dataKey }:{
+  data:any,
+  dataKey:any
+}) {
   return (
     <ResponsiveContainer width="100%" height={300}>
       <LineChart data={data}>
